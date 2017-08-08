3ebd26c86ec82e6f5cf127509cc23e01da14b5e4
pip36 <phil_23@live.co.uk>
2017-08-08 14:53:23 +0100

    set event listeners for paginated posts on load

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,16 +1,10 @@
 $(document).on('turbolinks:load',function(){
 
-  $('.comment-field').focus(function(){
-    if ($(this).val() == "write a comment..."){
-      $(this).val("");
-    };
-  });
+  reset_listeners();
 
-  $('.replies').css("display","none");
-  $('.reply-entry').css("display","none");
-  $('.comments').css("display", "none");
-  $('.comment-entry').css("display", "none");
+});
 
+var set_listeners = function(){
   $('.reply-link').click(function(){
     $(this).parents('.post-action-panel').parent().find('.replies').toggle();
     $(this).parents('.post-action-panel').parent().find('.replies').find('.replies').css("display", "none");
@@ -24,11 +18,22 @@ $(document).on('turbolinks:load',function(){
     $(this).parents('.post-action-panel').next().find('.comment-entry').first().toggle();
   });
 
+   $('.comment-field').focus(function(){
+     if ($(this).val() == "write a comment..."){
+       $(this).val("");
+     };
+   });
+}
 
-
-
-
-
-
-
-});
+var reset_listeners = function(){
+  $('.reply-link').off('click');
+  $('.comment-link').off('click');
+  $('.reply-post').off('click');
+  $('.comment-field').off('focus');
+  $('.replies').css("display","none");
+  $('.reply-entry').css("display","none");
+  $('.comments').css("display", "none");
+  $('.comment-entry').css("display", "none");
+  set_listeners();
+  console.log("reset");
+}
